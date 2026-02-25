2cb6cc63f57d13d865f23c08aa83f4a51b0c5cb0
NataschaBGB <nannabjerning@hotmail.com>
2026-02-25 10:40:16 +0100

    delete: removed old connect file

--- connectOLD.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    try {
-        $dbh = new PDO('mysql:host=localhost;dbname=league_of_legends', 'root', 'root');
-    }
-    catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-    }
